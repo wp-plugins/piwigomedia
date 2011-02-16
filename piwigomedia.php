b00d7535fc1de34f3f89475fa31c02696009bc2a
johnnyfive <johnnyfive@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-16 13:59:50 +0000

    - v0.8 - first import into WP Plugins SVN
    git-svn-id: https://plugins.svn.wordpress.org/piwigomedia/trunk@346422 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ piwigomedia.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: PiwigoMedia
+Plugin URI: http://joaoubaldo.com
+Description: This plugins allows media from a Piwigo gallery site to be inserted into WordPress posts.
+Version: 0.8
+Author: João C.
+Author URI: http://joaoubaldo.com
+License: GPL2 (see attached LICENSE file)
+*/
+
+require_once('piwigomedia_admin.php');
+
+function register_piwigomedia_tinymce_button($buttons) {
+    array_push($buttons, 'separator', 'piwigomedia');
+    return $buttons;
+}
+
+function register_piwigomedia_tinymce_plugin($plugin_array) {
+    $plugin_array['piwigomedia'] = WP_PLUGIN_URL . 
+        '/piwigomedia/tinymce/editor_plugin.js';
+    return $plugin_array;
+}
+
+function register_piwigomedia_plugin() {
+    if (!current_user_can('edit_posts') && !current_user_can('edit_pages'))
+        return;
+    if (get_user_option('rich_editing') != 'true')
+        return;
+
+    add_filter('mce_buttons', 'register_piwigomedia_tinymce_button');
+    add_filter('mce_external_plugins', 'register_piwigomedia_tinymce_plugin');
+    add_action('wp_head', 'load_piwigomedia_headers');
+    add_action('admin_head', 'load_piwigomedia_headers');
+}
+
+function load_piwigomedia_headers() {
+    echo "<link type=\"text/css\" rel=\"stylesheet\" href=\"".
+    WP_PLUGIN_URL."/piwigomedia/css/piwigomedia.css\"/>";
+}
+
+add_action('init', 'register_piwigomedia_plugin');
+?>
